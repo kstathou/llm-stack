6c9b4fe61f931f76998fc4ad677f2a051c1936bf
Kostas Stathoulopoulos <k.stathoylopoylos@gmail.com>
2023-12-02 14:16:23 +0000

    initial setup

--- src/llm_stack/dummy.py
+++ src/llm_stack/dummy.py
@@ -1,17 +1,16 @@
 import pandas as pd
 
 
-class Data:
+class Move:
     """Process data.
 
     Yes a long one.
-
-
     """
 
     def __init__(self, data: str) -> None:
-        """Do something nice"""
+        """Do something nice."""
         self.data = pd.read_csv(data)
+
         self.columns = list(data.columns)
         self.index = list(data.index)
 
