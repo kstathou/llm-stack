2c55af9cb315cffa60022f0ef91fdb428329cc9b
Kostas Stathoulopoulos <k.stathoylopoylos@gmail.com>
2023-12-02 14:04:14 +0000

    initial setup

---
+++ src/llm_stack/dummy.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+class Data:
+    """Process data.
+
+    Yes a long one.
+    """
+
+    def __init__(self, data: str) -> None:
+        """Do something nice"""
+        self.data = pd.read_csv(data)
+        self.columns = list(data.columns)
+        self.index = list(data.index)
+
+    def __getitem__(self, item: str) -> str:
+        """Do something nice squared."""
+        return self.data[item]
